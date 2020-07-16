5dc9708be961c0b57b065f09c03e9fdad89d6f93
lecuebas <lecuebas@gmail.com>
2020-07-16 20:37:58 -0300

    Questão 11 totalmente resolvida + explicações

--- Quiz1_Semana1.R
+++ Quiz1_Semana1.R
@@ -5,11 +5,14 @@
 #1ª etapa: dezipar o arquivo atribuindo ao objeto "dezip":
 
 temp1<-"C:/Users/Leticia/Desktop/datasciencecoursera/quiz1_data.zip"
-unzip(temp1,list=TRUE)
-unzip(temp1)
-dir()
+unzip(temp1,list=TRUE) #Verifica o que tem dentro do zip
+unzip(temp1) #dezipa
+dir() #verifica os arquivos presentes no diretório de trabalho
 
 #2ª etapa: abrir o arquivo csv atribuindo-o a outro objeto:
 
 quiz_table <- read.csv(file = 'hw1_data.csv')
-head(quiz_table)
+
+#3ª etapa: verificar o cabeçalho da tabela:
+head(quiz_table) #OU
+colnames(quiz_table)
